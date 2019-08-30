3dd6407430a11329da24924e34a4a1bde64b5ffb
Vishwanath Bondugula <40670894+vishwa5854@users.noreply.github.com>
2019-08-31 01:08:45 +0530

    optimised code to a greater extent

--- Main.java
+++ Main.java
@@ -6,66 +6,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Enter the String : ");
+        System.out.print("Enter a string : ");
         Scanner scanner = new Scanner(System.in);
         String lol = scanner.nextLine();
         ArrayList<String> input = new ArrayList<>();
         for(int i=0;i<lol.length();i++){
             input.add(lol.substring(i,i+1));
         }
-        process(lol, input);
-        ArrayList<String> output = new ArrayList<>();
-        removeRedundancies(input,lol,output);
-
-    }
-
-    private static void removeRedundancies(ArrayList<String> input,String lol,ArrayList<String> output){
+        process(lol,input);
         for(String a : input){
-            if(a.length() == lol.length()){
-                output.add(a);
-            }
-        }
-        String[] temp = new String[output.size()];
-        for(int i=0;i<output.size();i++){
-            temp[i] = output.get(i);
-        }
-        removeRedundantAttributes(output, temp);
-        for(String a : output){
             System.out.println(a);
         }
-    }
-
-    private static void removeRedundantAttributes(ArrayList<String> output, String[] temp) {
-        for (String s : temp) {
-            int count = 0;
-            for(int i=0;i<s.length();i++){
-                for(int j=0;j<s.length();j++){
-                    if(s.charAt(i) == s.charAt(j)){
-                        count++;
-                    }
-                }
-            }
-            if(count > 4){
-                output.remove(s);
-            }
-        }
+        System.out.println(input.size());
     }
 
     private static void process(String lol, ArrayList<String> input) {
-        int start = 0;
-        int stop = lol.length();
         for(int i=0;i<lol.length()-1;i++) {
-            generateTwoAtATime(start,stop,input,lol.length());
-            start = stop;
-            stop = input.size();
+            int temp = input.size();
+            generateTwoAtATime(temp,input,lol.length(),lol);
+            if (temp > 0) {
+                input.subList(0, temp).clear();
+            }
         }
     }
 
-    private static void generateTwoAtATime(int start,int end,ArrayList<String> med,int initial) {
-        for(int i=start;i<end;i++){
+    private static void generateTwoAtATime(int end, ArrayList<String> input, int initial, String lol) {
+        for(int i = 0; i<end; i++){
             for(int j=0;j<initial;j++){
-                med.add(med.get(i) + med.get(j));
+                if(!contains(input.get(i),lol.charAt(j))) {
+                    input.add(input.get(i) + lol.charAt(j));
+                }
             }
         }
+    }
+
+    private static boolean contains(String a, char b){
+        int count = 0;
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i) == b){
+                count++;
+            }
+        }
+        return count > 0;
     }
 }
